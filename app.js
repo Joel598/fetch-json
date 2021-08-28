fb4064aa6c8363ea17a09aab518f9d516d49d11f
Joel Dsouza <44874750+Joel598@users.noreply.github.com>
2021-08-28 18:46:13 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const request = require("request");
-
-request({
-	url:"https://raw.githubusercontent.com/Joel598/api-reactjs/main/src/data/posts.json",
-	json:true
-}, (err, response, body) => {
-	console.log(body);
-});
